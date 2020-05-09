b2e30fb2decb6e808c051ae0a15aecd0c3330f36
NSaiAkshara <34544810+NSaiAkshara@users.noreply.github.com>
2020-05-09 18:12:46 +0530

    Update cal.java

--- src/main/java/cal.java
+++ src/main/java/cal.java
@@ -6,39 +6,22 @@ public class cal {
     public static double sub(double a,double b){
     	return a>b?a-b:b-a;
     }
+    public static double mul(double a,double b){
+    	return a*b;
+    }
+    public static double div(double a,double b){
+    	return a/b;
+    }
     public static void main(String args[]) {
-    	double x, y, sum;
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println("Enter first number:");
-    	x = sc.nextDouble();
-    	System.out.println("Enter second number:");
-    	y = sc.nextDouble();
-        System.out.println("Choose from the following options:");
-        int ch;
-        System.out.println("1 to add");
-        System.out.println("2 to subtract");
-        System.out.println("3 to multiply");
-        System.out.println("4 to divide");
-        ch = sc.nextInt();
-        if(ch==1){
-		sum = add(x,y);
-    		System.out.println("Sum is: " + sum);
-        }
-    	else if(ch==2){
-		sum = sub(x,y);
-    		System.out.println("Sum is: " + sum);
-        }
-	else if(ch==3){
-		sum = add(x,y);
-    		System.out.println("Sum is: " + sum);
-        }
-	else if(ch==4){
-		sum = sub(x,y);
-    		System.out.println("Sum is: " + sum);
-        }
-	else{
-		System.out.println("You chose an invalid input");
-	}
+    	double x=5.2, y=3.6, result;
+	result = add(x,y);
+    	System.out.println("Add 5.2 and 3.6 = " + result);
+    	result = sub(x,y);
+    	System.out.println("Sub 5.2 and 3.6 = " + result);
+	result = mul(x,y);
+    	System.out.println("Mul 5.2 and 3.6 = " + result);
+	result = div(x,y);
+    	System.out.println("Div 5.2 and 3.6 = " + result);
     }
 }
 
